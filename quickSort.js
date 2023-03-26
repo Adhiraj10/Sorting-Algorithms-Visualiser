47d1a771f4b50437a1f1de67d4f5ae1d2c10db6a
Adhiraj10 <soparkaradhiraj@gmail.com>
2023-03-26 18:13:44 +0530

    quickSort fixed

--- quickSort.js
+++ quickSort.js
@@ -1,52 +1,48 @@
-// Needs fixing
 const quickBtn = document.getElementById('quick');
 
-const partition = async (bars, l, r) => {
-   let pivot = bars[l];
+
+const partition = async (bars, low, high) => {
+   let pivot = bars[low];
+   let i = low + 1, j = high;
+
    pivot.style.backgroundColor = '#ff0000';
-   let i = l, j = r;
-   while (i < j) {
-      do {
-         i++;
+
+   while (i <= j) {
+      while (i <= high && parseInt(bars[i].style.height) <= parseInt(pivot.style.height)) {
          bars[i].style.backgroundColor = 'yellow';
          await animater((1110 - parseInt(animationSpeed.value)));
-      } while (parseInt(bars[i].style.height) < parseInt(pivot.style.height));
-
-      do {
-         j--;
+         bars[i].style.backgroundColor = '#006400';
+         i++;
+      }
+      while (j >= low && parseInt(bars[j].style.height) > parseInt(pivot.style.height)) {
          bars[j].style.backgroundColor = 'yellow';
          await animater((1110 - parseInt(animationSpeed.value)));
-      } while (parseInt(bars[j].style.height) > parseInt(pivot.style.height));
-
+         bars[j].style.backgroundColor = '#006400';
+         j--;
+      }
       if (i < j) {
-         await animater((1110 - parseInt(animationSpeed.value)));
          bars[i].style.backgroundColor = 'pink';
          bars[j].style.backgroundColor = 'pink';
          await animater((1110 - parseInt(animationSpeed.value)));
          swap(bars[i], bars[j]);
-
-         for (let k = l + 1; k < i; k++)
-            bars[k].style.backgroundColor = '#006400';
-
-         for (let k = j; k < bars.length - 1; k++)
-            bars[k].style.backgroundColor = '#006400';
-
-         await animater((1110 - parseInt(animationSpeed.value)));
-         bars[i].style.backgroundColor = '#006400';
-         bars[j].style.backgroundColor = '#006400';
       }
    }
-   animater((1110 - parseInt(animationSpeed.value)));
-   swap(bars[l], bars[j]);
+
+   await animater((1110 - parseInt(animationSpeed.value)));
+   swap(bars[j], bars[low]);
+   pivot.style.backgroundColor = '#006400';
+
+   for (let k = low + 1; k <= j - 1; k++)
+      bars[k].style.backgroundColor = '#006400';
    await recolor();
    return j;
 }
 
-const quickSort = async (bars, l, r) => {
-   if (l < r) {
-      let pivot = await partition(bars, l, r);
-      await quickSort(bars, l, pivot);
-      await quickSort(bars, pivot + 1, r);
+const quickSort = async (bars, low, high) => {
+   if (low < high) {
+      let pivot = await partition(bars, low, high);
+      await quickSort(bars, low, pivot - 1);
+      await quickSort(bars, pivot + 1, high);
    }
 }
 
